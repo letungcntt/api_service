5c50245c792133c4c42a2dc54c05a17913104e1e
Thanh Tùng <letungcntt@gmail.com>
2020-09-28 15:39:10 +0700

    update certicate

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,10 +5,10 @@
 use Mix.Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||
+  System.get_env("hello_dev") ||
     raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
+    environment variable hello_dev is missing.
+    For example: ecto://postgre:a@local/hello_dev
     """
 
 config :api_service, ApiService.Repo,
